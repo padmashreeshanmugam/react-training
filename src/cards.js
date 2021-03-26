d445ee7478d1c5b07b4c892c477b5eca7f6df1a0
Padmashree Shanmugasundaram <padmashree.s@ameexusa.net>
2021-03-26 22:49:02 +0530

    my first react exercise

---
+++ src/cards.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './cards.css'
+
+const arrayDatas=[
+    {id:123 ,imageUrl :"./product-images/load_more_img1.png" , price :600 ,title: "Lorem Impus1" , desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."},
+    {id:124 ,imageUrl :"./product-images/load_more_img2.png" , price :580 ,title: "Lorem Impus2" , desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."},
+    {id:125 ,imageUrl :"./product-images/load_more_img5.png" , price :570 ,title: "Lorem Impus3", desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old." },
+    {id:126 ,imageUrl :"./product-images/load_more_img6.png" , price :580 ,title: "Lorem Impus4" , desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."},
+    {id:127 ,imageUrl :"./product-images/load_more_img2.png" , price :580 ,title: "Lorem Impus2" , desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."},
+    {id:128 ,imageUrl :"./product-images/load_more_img5.png" , price :570 ,title: "Lorem Impus3", desc:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old." }
+
+];
+class cards extends React.Component{
+    render(){
+        return(
+            arrayDatas.map(item =>(
+                <div className="card" key={item.id} >
+                   <img src={item.imageUrl} alt="img"/>
+                   <h2 className="item_title">{item.title}</h2> 
+                   <p className="item_price"> <strong>€ {item.price}</strong> </p>
+                   <p>{item.desc}</p>
+                   <button>Add to Cart</button>
+                </div>
+            ))           
+        )
+    };
+
+};
+export default cards;
---
+++ src/components/toDoList/List.js
@@ -0,0 +1,39 @@
+// import React from 'react';
+
+
+// class List  extends React.Component{
+//     state ={
+//         inputValue: "",
+//         rows :[]
+//     }
+//     handleClick () {
+//         var rows =this.state.rows;
+//         rows.push(this.state.inputValue);
+//         this.setState({
+//             rows :rows
+//         })
+//     }
+//     handlechange(){
+//        this.setState({
+//            rows :this.state.rows
+//        }) 
+//     }
+       
+      
+//     render(){
+//         return(
+//             <div>
+//                 <input type="text" id="input" value={this.state.inputValue} onChange={this.handleChange()}/>
+//                 <input
+//                 type="button"
+//                 value="Alert the text input"
+//                 onClick={this.handleClick()}
+//                 />
+                
+//             </div>
+        
+
+//         )
+//     };
+// }
+// export default List;
